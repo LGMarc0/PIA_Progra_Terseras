20b16c17ce486995008cd817c9ff72f35197ad77
LGMarco <marcologra@gmail.com>
2025-09-29 19:58:16 -0600

    Menu de opciones

--- Empleados.py
+++ Empleados.py
@@ -6,3 +6,18 @@ class Empleado:
         self.fecha_ingreso = fechas_ingreso
         self.salario = salario
         self.bono = bono
+
+while True:
+   print("\n--- MENÚ ---")
+   print("Elige una opcion")
+   print("1. Agregar Un Empleado")
+   print("2.- Ver info. de un Empleado")
+   print("3.- Ver Todos los Empleados")
+   print("4.- Eliminar Empleado") 
+   print("5.- Modificar info. de un Empleado")
+   print("6.- Salir")
+   try:
+        opcion = int(input("¿Qué opción desea elegir? "))
+   except ValueError:
+        print("Por favor, ingrese un número válido.")
+        continue
