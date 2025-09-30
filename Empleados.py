b9abee983e26f32dd0d26e884813e8938eba0625
LGMarco <marcologra@gmail.com>
2025-09-29 19:44:57 -0600

    clase empleados

---
+++ Empleados.py
@@ -0,0 +1,8 @@
+class Empleado:
+    def __init__(self, departamento, puesto, horas_lab, fechas_ingreso, salario, bono):
+        self.departamento = departamento
+        self.puesto = puesto
+        self.horas_lab = horas_lab
+        self.fecha_ingreso = fechas_ingreso
+        self.salario = salario
+        self.bono = bono
